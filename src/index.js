b9edbcaa1ef77d7e63e0e041c07baa73565d30fc
agabriela10 <angie.gabriela.sasa@gmail.com>
2020-01-31 01:08:14 -0500

    the final changes

--- src/index.js
+++ src/index.js
@@ -1,83 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import subs from "./subscribers.json";
+import "./index.css";
 
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+let users = subs;
 
-const data = [
-  {
-    name: "Pickle",
-    lastName: "Rick",
-    age: "65",
-    sport: "Kill rats"
-  },
-  {
-    name: "Artur",
-    lastName: "King",
-    age: "23",
-    sport: "Run"
-  },
-  {
-    name: "Bob",
-    lastName: "Maley",
-    age: "99",
-    sport: "Soccer"
-  },
-  {
-    name: "Pink",
-    lastName: "Panter",
-    age: "45",
-    sport: "Basket"
+class AppTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {users};
+    
+        this.compareBy.bind(this);
+        this.sortBy.bind(this);
+    }
+
+  compareBy(key) {
+    return function (a, b) {
+      if (a[key] < b[key]) return -1;
+      if (a[key] > b[key]) return 1;
+      return 0;
+    };
   }
-];
+ 
+  sortBy(key) {
+    let usersCopy = [...this.state.users];
+    usersCopy.sort(this.compareBy(key));
+    this.setState({users: usersCopy});
+  }
 
-function AppTable() {
-
-    return (
-        <div className="App">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12 text-md-left align-self-center">
-                            <table class="table text-center">
-                                <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Last Name</th>
-                                        <th>Age</th>
-                                        <th>Sport</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Pickle</td>
-                                        <td>Rick</td>
-                                        <td>65</td>
-                                        <td>Kill rats</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-        
-                <div class="container">
-                    <div class="row">
-                        <div class="col-4">
-                            <button>sortByName</button>
-                        </div>
-                        <div class="col-4">
-                            <button>sortByAge</button>
-                        </div>
-                        <div class="col-4">
-                            <button>sortBySport</button>
-                        </div>
-                    </div>
-                </div>
+  render() {
+  return (
+    <div>
+        <table id= "users">
+        <thead>
+            <tr>
+            <th>Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+            <th>Sport</th>
+            </tr>
+        </thead>
+        <tbody>
+            {this.state.users.map(e => (
+            <tr>
+                <td>{e.name}</td>
+                <td>{e.lastName}</td>
+                <td>{e.age}</td>
+                <td>{e.sport}</td>
+            </tr>
+            ))}
+        </tbody>
+        </table>
+        <div id= "buttonSort">
+            <button onClick={() => this.sortBy('name')}>sortByName</button>
+            <button onClick={() => this.sortBy('age')}>sortByAge</button>
+            <button onClick={() => this.sortBy('sport')}>sortBySport</button>
         </div>
-    );
+    </div>
+  );
+}
 }
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 
 ReactDOM.render(<AppTable />, container);
